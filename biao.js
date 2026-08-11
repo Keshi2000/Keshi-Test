1f7766cc6616bd9a3a0af5c9121c8bf565086112
Keshi2000 <kesh911@qq.com>
2026-08-12 04:11:48 +0800

    更新 biao.js

--- biao.js
+++ biao.js
@@ -1,19 +1,5 @@
-/*
-脚本名称：游戏数据修改
-更新时间：2026-08-12
-电报频道：https://t.me/GieGie777
-使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
-*******************************
-[rewrite_local]
-^https?:\/\/javelin.mandrillvr.com\/api\/data\/get_game_data url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/bqwz.js
-
-[mitm]
-hostname = javelin.mandrillvr.com
-*/
-
 let body = $response.body;
 
-// ==================== 基础资源修改 ====================
 // 金币
 body = body.replace(/\\"coin\\":\d+/g, '\\"coin\\":9999880');
 // 钻石
@@ -25,8 +11,7 @@ body = body.replace(/\\"rank_ticket\\":\d+/g, '\\"rank_ticket\\":666');
 // PVE体力
 body = body.replace(/\\"pve_power\\":\d+/g, '\\"pve_power\\":888');
 
-// ==================== 英雄碎片全部改为999 ====================
-// 您当前拥有的所有碎片ID（基于您提供的数据）
+// 英雄碎片全部改为999
 var fragmentIds = [
     '20002','20003','20006','20009','20011','20013','20015',
     '20016','20017','20018','20019','20020','20021','20022',
@@ -35,13 +20,10 @@ var fragmentIds = [
     '20043','20044','20045'
 ];
 
-// 构建替换字符串
 var fragStr = fragmentIds.map(function(id) {
     return '\\"' + id + '\\":999';
 }).join(',');
 
-// 执行替换
 body = body.replace(/\\"hero_fragment\\":\{[^}]*\}/, '\\"hero_fragment\\":{' + fragStr + '}');
 
-// ==================== 返回修改后的数据 ====================
 $done({ body });
